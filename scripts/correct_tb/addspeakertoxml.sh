43611be514c46c15cd991f037f08a1aa3eaa0062
Francesco Mambrini <f.mambrini@gmail.com>
2020-09-21 15:01:43 +0200

    OT

--- scripts/correct_tb/addspeakertoxml.sh
+++ scripts/correct_tb/addspeakertoxml.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-FS='!/home/francesco/Documents/work/Nextcloud/Documents/Projects/Daphne/data/annotation/latest/tlg0085/tlg003/*.xml'
+FS='/home/francesco/Desktop/tlg0011.tlg004.daphne_tb-grc1.xml'
 
 udapy read.Agldt files="$FS" \
   agldt.agldt_util.SetSpeakers \
   util.Eval doc='doc.meta["docname"]=doc.meta["loaded_from"].split("/")[-1]' \
-  .WriteDaphne docname_as_file=1 includeSpeakers=1
+  .WriteDaphne docname_as_file=1 includeSpeakers=1  
